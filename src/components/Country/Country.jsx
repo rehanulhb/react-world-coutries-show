3c6e168de7269fbab8008d0e2ebf97f0ca8e94f2
rehanulhb <rehanulh@gmail.com>
2024-10-26 23:19:38 +0200

    Added code for SHow the DIV colored if Visited using conditional CSS

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
     const {name, flags, population, area} = country;
 
     const [visited, setVisited] = useState(false);
@@ -11,8 +11,8 @@ const Country = ({country}) => {
     }
 
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited && 'visited' }`}>
+            <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population : {population} </p>
             <p>Area : {area}</p>

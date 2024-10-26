6c565e6d1ef797bfe43169925c91bc2a214afbbe
rehanulhb <rehanulh@gmail.com>
2024-10-26 23:40:47 +0200

    Working on Showing the Visited Country on Top

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './Country.css';
 
-const Country = ({ country }) => {
-    const {name, flags, population, area} = country;
+const Country = ({ country, handleVisitedCountry }) => {
+    const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
 
@@ -10,12 +10,18 @@ const Country = ({ country }) => {
         setVisited(!visited);
     }
 
+    
+    console.log(handleVisitedCountry);
+
     return (
         <div className={`country ${visited && 'visited' }`}>
-            <h3>Name: {name.common}</h3>
+            <h3 style={{color: visited ? 'purple' : 'black'}}>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population : {population} </p>
             <p>Area : {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button>Mark Visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited this country' : 'I Want to Visit'}
         </div>
